26d11ef1afc1e39a5da07f8cd93d97ad3e4049f8
MD. SAFAYET HOSSAIN SADHIN <sadhinv@gmail.com>
2026-09-09 21:02:34 +0600

    server.ts updated

--- src/server.ts
+++ src/server.ts
@@ -11,51 +11,27 @@ import {
 
 const PORT = config.port || process.env.PORT || 5000;
 
-let isInitialized = false;
-
 const main = async () => {
-	if (isInitialized) return;
-
 	try {
 		await prisma.$connect();
 		console.log("Connected to the database successfully.");
-
-		if (!redisClient.isOpen) {
-			await redisClient.connect();
-			console.log("Redis Connected Successfully.");
-		}
-
+		await redisClient.connect();
+		console.log("Redis connected successfully.");
 		await transporter.verify();
-		console.log("Nodemailer Connected Successfully.");
+		console.log("nodemailer connected successfully");
 
 		await seedTesterAdmin();
 		await seedTesterClient();
 		await seedTesterProfessional();
 
-		isInitialized = true;
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
 	} catch (error) {
 		console.error("Error starting the server:", error);
 		await prisma.$disconnect();
-
-		throw error;
+		process.exit(1);
 	}
 };
 
-app.use(async (req, res, next) => {
-	try {
-		await main();
-		next();
-	} catch (error) {
-		res.status(500).json({
-			error: "Internal Server Error",
-			message: "Failed to initialize background connections.",
-		});
-	}
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-
-export default app;
+main();
